e0e0dd6866e43aec50fd297f6b352ef134d60839
tanykazy <tanykazy>
2023-09-20 23:24:00 +0900

    内部で使用する引数の型を定義

--- Calendar.js
+++ Calendar.js
@@ -1,3 +1,13 @@
+/**
+ * @typedef {object} CalendarParam - カレンダーの名前とID
+ * @property {string} name - カレンダーの名前
+ * @property {string} id - カレンダーのID
+ */
+
+/**
+ * ユーザーがオーナーまたは登録しているすべてのカレンダーを取得する
+ * @returns {CalendarParam} カレンダーの
+ */
 function getAllCalendars() {
     const calendars = CalendarApp.getAllCalendars();
 
