670242b94f3cfbdfc1ea8698b2c50f74d85a5f3b
t-cool <t-cool@t-coolnoMacBook-Pro.local>
2023-09-22 11:51:11 +0900

    ドキュメントにレポートを書き出す

--- Export.js
+++ Export.js
@@ -1,6 +1,6 @@
 /**
  * カレンダーの内容をドキュメントにエクスポートする
- * @param {GoogleAppsScript.Calendar} calendar - カレンダー選択UIで選択されたカレンダーのID
+ * @param {GoogleAppsScript.Calendar} calendar - カレンダー選択UIで選択されたカレンダー
  * @param {Date | null} from - 期間の開始日付
  * @param {Date | null} to - 期間の終了日付
  * @returns {string} カレンダーのデータを書き出したドキュメントファイルのURL
@@ -10,7 +10,70 @@ function exportDocs(calendar, from, to) {
     console.log(from);
     console.log(to);
 
-    // do something...
+    return getReportURL(calendar, from, to);
+}
 
-    return docsUrl;
+/**
+ * カレンダーから指定された期間のイベントを取得し、レポートのドキュメントを作成する
+ * @param {GoogleAppsScript.Calendar} calendar - 選択されたカレンダー
+ * @param {Date | null} from - 期間の開始日付
+ * @param {Date | null} to - 期間の終了日付
+ * @returns {GoogleAppsScript.Drive.File} 作成されたドキュメントのファイル
+ */
+function createReport(calendar, from, to) {
+    // 新規Googleドキュメントを作成
+    const doc = DocumentApp.create("カレンダーレポート_" + new Date().toISOString());
+    const body = doc.getBody();
+    
+    // 文書の内容を追加
+    body.appendParagraph(`カレンダーレポート: ${calendar.getName()} \n期間: ${from ? from.toLocaleDateString() : "開始日未指定"} から ${to ? to.toLocaleDateString() : "終了日未指定"}\n`);
+
+    // カレンダーからイベントを取得
+    const events = calendar.getEvents(from, to);
+    
+    const eventData = events.map(event => {
+        return [event.getStartTime().toLocaleDateString(), event.getTitle(), event.getDescription() || ""];
+    });
+
+    // ヘッダー情報を追加
+    const data = [["日時", "内容", "詳細"], ...eventData];
+
+    body.appendTable(data);
+
+    return DriveApp.getFileById(doc.getId());
+}
+
+/**
+ * レポートのドキュメントのURLを取得する
+ * @param {GoogleAppsScript.Calendar} calendar - カレンダー選択UIで選択されたカレンダー
+ * @param {Date | null} from - 期間の開始日付
+ * @param {Date | null} to - 期間の終了日付
+ * @returns {string} レポートのドキュメントのURL
+ */
+function getReportURL(calendar, from, to) {
+  const report = createReport(calendar, from, to);
+  return report.getUrl();
+}
+
+/**
+ * レポートを指定のフォルダに保存する
+ * @returns {GoogleAppsScript.Drive.File} フォルダに移動されたドキュメントのファイル
+ */
+function createReportInFolder() {
+  const folderName = "calendarReport";
+  let folder;
+  
+  // フォルダの存在確認および作成
+  const folders = DriveApp.getFoldersByName(folderName);
+  if (folders.hasNext()) {
+    folder = folders.next();
+  } else {
+    folder = DriveApp.createFolder(folderName);
+  }
+
+  // レポートのドキュメントを作成および移動
+  const report = createReport();
+  report.moveTo(folder);
+
+  return report;
 }
